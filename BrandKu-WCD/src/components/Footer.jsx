2ca21ed17a091fdfced3347ae2a6e6c615f2e65f
LeroyChris <leroy.christopher.g@gmail.com>
2026-06-14 20:39:33 +0700

    chore: create component from html and js logic

---
+++ BrandKu-WCD/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center py-6 px-6 bg-white border-t border-gray-200 text-gray-500 text-sm">
+      <p>&copy; 2025 BrandKu. All rights reserved.</p>
+    </footer>
+  );
+}
